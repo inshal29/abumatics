d6fc2ae5ed5c703f75f8d139a7ce472a35ec0294
Abdul Majeed <84311017+Majeed-hub@users.noreply.github.com>
2025-01-22 00:20:22 +0530

    linked contact us with hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,7 +39,7 @@ export const Hero = () => {
 
         {/* Button Section */}
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Contact us</Button>
+          <Button className="w-full md:w-1/3"><a href="#contact-us">Contact us</a></Button>
         </div>
       </div>
 
